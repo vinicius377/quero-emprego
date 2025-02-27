326a5d5a4de8ebcb41465925135447a4860a04c7
Vinicius <viniciusep181@gmail.com>
2025-02-26 22:25:00 -0300

    feat: add business service layer

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -3,6 +3,15 @@ import { trpc } from './lib/trpc';
 
 const App = () => {
 
+  trpc.business.createBusiness.mutate({
+    location: {
+      city: "tetse"
+    },
+    cnpj: 123123,
+    phoneNumber: 123123,
+    businessName: "asdasd",
+    responsableName: "aaaa"
+  })
   return (
     <div className="content">
       <h1>Rsbuild with React</h1>
